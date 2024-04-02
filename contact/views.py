44a8578ab1fca181683a0bacd413959b3983e080
GDV373 <georgeattard144@gmail.com>
2024-04-02 17:17:50 +0000

    added contuct us v12 update view to use allauth serting

--- contact/views.py
+++ contact/views.py
@@ -2,27 +2,23 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import ContactUsForm
-from allauth.account.models import EmailAddress
-from allauth.account.utils import send_email_confirmation
-from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 
 def contact_us(request):
     if request.method == 'POST':
         form = ContactUsForm(request.POST)
         if form.is_valid():
             form.save()
-            
-            # Send email confirmation to the user if email is provided
-            email = form.cleaned_data['email']
-            if email:
-                user_model = get_user_model()
-                user = user_model.objects.filter(email=email).first()
-                if user:
-                    if not EmailAddress.objects.filter(user=user, verified=True).exists():
-                        send_email_confirmation(request, user)
-                        
-            messages.success(request, 'Your message has been sent! test')
-            return HttpResponseRedirect(reverse('contact_us') + '?submitted=True')
+
+            # Send email to your email address
+            subject = 'New message from your website'
+            message = f'Name: {form.cleaned_data["name"]}\nEmail: {form.cleaned_data["email"]}\nSubject: {form.cleaned_data["subject"]}\nMessage: {form.cleaned_data["message"]}'
+            sender_email = form.cleaned_data['email']
+            recipient_list = [settings.EMAIL_HOST_USER] 
+            send_mail(subject, message, sender_email, recipient_list)
+
+            messages.success(request, 'Your message has been sent! We will reply ASAP!')
+            return HttpResponseRedirect('/contact?submitted=True')
         else:
             messages.warning(request, 'Message not sent. Please try again.')
     
