e22d1472cf20239f55a51b59b1318dec05bfb243
GDV373 <georgeattard144@gmail.com>
2024-04-03 09:59:11 +0000

    added wish list v4

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.db.models.functions import Lower
+from django.http import HttpResponseBadRequest
+from .models import Product, WishItem
 
-from .models import Product, Category
-from .forms import ProductForm
-
-from .models import WishItem
-
+@login_required
 def wish_list(request):
     # Fetch wish items associated with the current user
     wish_items = WishItem.objects.filter(user=request.user)
+    return render(request, 'wishlist/wishlist.html', {'wish_items': wish_items})
 
-    # Fetch all available products
-    all_products = Product.objects.all()
-
-    return render(request, 'wishlist/wish_list.html', {'wish_items': wish_items, 'all_products': all_products})
-
+@login_required
 def add_to_wishlist(request):
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
-        user = request.user
-        # Create a WishItem instance for the selected product and current user
-        WishItem.objects.create(user=user, product_id=product_id)
-    return redirect('wish_list')
+        if product_id:
+            product = get_object_or_404(Product, pk=product_id)
+            # Create a WishItem instance for the selected product and current user
+            WishItem.objects.create(user=request.user, product=product)
+            messages.success(request, 'Product added to wish list successfully.')
+            return redirect('wish_list')
+    # Handle invalid or missing product_id
+    return HttpResponseBadRequest('Invalid request or missing product ID.')
