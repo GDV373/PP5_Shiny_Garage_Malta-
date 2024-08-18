24b540e9b95a3876c335548062634f9766ce7893
George Attard <georgeattard144@gmail.com>
2024-08-18 18:53:00 +0000

    revert to basic chat bot revert urls

--- ShinyGarageMalta/urls.py
+++ ShinyGarageMalta/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import chat_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,7 +30,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')), 
     path('contact/', include('contact.urls')),
-    path('chatbot/', include('chatbot.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'ShinyGarageMalta.views.handler404'
