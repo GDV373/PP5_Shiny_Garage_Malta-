c98538be4f3965228acec037c6676c210bf064f1
George Attard <georgeattard144@gmail.com>
2024-09-22 21:08:12 +0000

    New Model discount bug fix v4.6

--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
+from django.utils import timezone
 from django_countries.fields import CountryField
 
 from products.models import Product
